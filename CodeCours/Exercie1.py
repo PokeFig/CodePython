199f4228a9a3df1276d12a765474293e96888e0d
albatackk <bahunemail2@gmail.com>
2024-01-03 15:06:14 +0100

    CodeTest

---
+++ CodeCours/Exercie1.py
@@ -0,0 +1,11 @@
+######################################################
+##                  Palette_Hauteur                 ##
+######################################################
+
+
+#Demande valeur utilisateur
+def ConfigUti():
+NbrCanette = input('Nombre de canette entre 1 et 100 :')
+
+
+
