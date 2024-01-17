f366bb1e6a0f75681d7eea0137ba672ea7d004c1
Mister-J23 <indrichjardel@gmail.com>
2024-01-17 17:11:31 +0100

    j

--- CodeInProgressFich/Main.py
+++ CodeInProgressFich/Main.py
@@ -16,7 +16,7 @@ from Production import SaveProductImage
 from Production import Product
 from Production import getManufOrderToDo
 
-models_proxy = Connect(server_ip="172.31.10.65", server_port=8069, password="Ntm123456789!",)
+models_proxy = Connect(server_ip="172.31.10.64", server_port=8069, password="Ntm123456789!",)
 
 #======================================================================
 #Déclaration de varaibles global
@@ -25,24 +25,23 @@ models_proxy = Connect(server_ip="172.31.10.65", server_port=8069, password="Ntm
 gUid = None
 gUrl = None
 password = "Ntm123456789!"
-database = "PokeFigDataBase"
-numéro_produit = 47                                                                                                                  # Sélection du nuémro de produit
+database = "PokeFigDataBase"                                                                                                              
 
 #======================================================================
 if __name__ == "__main__":
 
-    #if models_proxy:
+    if models_proxy:
 
-        #products = Product(models_proxy, 20, password, database)                                                                      # Récupération de tous les produits de la BDD (ID, Nom, Prix)
+        products = Product(models_proxy, 20, password, database)                                                                      # Récupération de tous les produits de la BDD (ID, Nom, Prix)
         
         
-        #if products:
+        if products:
 
-            #for product in products:                                                                                                  # Boucle pour écriture la liste dans la console
-                #print(f"ID: {product.get('id')}")
+            for product in products:                                                                                                  # Boucle pour écriture la liste dans la console
+                print(f"ID: {product.get('id')}")
+                SaveProductImage(models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                   # Enregistrement de l'image demandée
 
-    #SaveProductImage(models_proxy, "PokeFigDataBase", 20, password, numéro_produit)                                                   # Enregistrement de l'image demandée
         
-    getManufOrderToDo(models_proxy)
+    #getManufOrderToDo(models_proxy)
 
 #======================================================================
