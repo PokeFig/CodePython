405263f28d21d46dcd0eade65bf386edee18ef5d
Paimblanc <paimblancleo@gmail.com>
2024-01-18 11:41:05 +0100

    maj

--- CodeInProgressFich/Main.py
+++ CodeInProgressFich/Main.py
@@ -12,24 +12,8 @@
 #======================================================================
 
 from odoo import Connect
-<<<<<<< HEAD
-#from Production import SaveProductImage
-#from Production import Product
-#from Production import getManufOrderToDo
-#from Production import createManufOrder
 import Production
-=======
-<<<<<<< HEAD
-from Production1copy import ShowProductImage
-from Production1copy import Product
-#from Production1 import getManufOrderToDo
-=======
-from Production import SaveProductImage
-from Production import Product
-from Production import getManufOrderToDo
-from Production import createManufOrder
->>>>>>> 2ad032a2bcf2ae622ea5bfe59d1b000b469b6175
->>>>>>> 83b9fb3f3f1ed08533bd1e7d4806618bbb160094
+
 
 models_proxy = Connect(server_ip="172.31.10.64", server_port=8069, password="Ntm123456789!",)
 
@@ -41,6 +25,7 @@ password = "Ntm123456789!"
 database = "PokeFigDataBase"
 order_id = 27     
 quantity = 10                                                                                      
+product_id = 57
 
 #======================================================================
 if __name__ == "__main__":
@@ -52,19 +37,17 @@ if __name__ == "__main__":
         
         #if products:
 
-<<<<<<< HEAD
-            for product in products:                                                                                                  # Boucle pour écriture la liste dans la console
-                print(f"ID: {product.get('id')}")
-                ShowProductImage(, models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                   # Enregistrement de l'image demandée
-=======
+            #for product in products:                                                                                                  # Boucle pour écriture la liste dans la console
+                #print(f"ID: {product.get('id')}")
+                #ShowProductImage(, models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                   # Enregistrement de l'image demandée
+
             #for product in products:                                                                                                  # Boucle pour écriture la liste dans la console
                 #print(f"ID: {product.get('id')}")
                 #SaveProductImage(models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                   # Enregistrement de l'image demandée
->>>>>>> 2ad032a2bcf2ae622ea5bfe59d1b000b469b6175
 
         
      Production.getManufOrderToDo(models_proxy)                                                                      #Récupération des OF
-     Production.createManufOrder(models_proxy, quantity, product_id=57)                                           #Création des OF
+     Production.createManufOrder(models_proxy, quantity, product_id)                                           #Création des OF
 
      #Production.confirmManufOrder(models_proxy,order_id)                                                             #confirmation de l'OF
      #Production.DoneManufOrder(models_proxy, order_id, quantity)                                                                 #Ordre de fabrication terminé
