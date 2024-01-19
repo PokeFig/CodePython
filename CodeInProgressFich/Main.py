92b3db3e67bfff698fbccfe0272463ce7c2bdd98
Paimblanc <paimblancleo@gmail.com>
2024-01-19 11:19:40 +0100

    valid

--- CodeInProgressFich/Main.py
+++ CodeInProgressFich/Main.py
@@ -24,18 +24,18 @@ models_proxy = Connect(server_ip="172.31.10.64", server_port=8069, password="Ntm
 
 password = "Ntm123456789!"
 database = "PokeFigDataBase"
-order_id = 34     
-quantity = 15                                                                                      
-product_id = 55
+order_id = 34                   #Numéro de l'of pour modif
+quantity = 15                   #QUAntitée qu'on veux produire
+product_id = 55                 #Numéro de produit
 
 #======================================================================
 if __name__ == "__main__":
 
     if models_proxy:
      
-        #ShowProductImage(, models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                   # Enregistrement de l'image demandée
+     #ShowProductImage(, models_proxy, "PokeFigDataBase", 20, password, product.get('id'))                                                       # Enregistrement de l'image demandée
         
-     Production.getManufOrderToDo(models_proxy)                                                                                                  #Récupération des OF
+     #Production.getManufOrderToDo(models_proxy)                                                                                                 #Récupération des OF
      #Production.createManufOrder(models_proxy, quantity, product_id)                                                                            #Création des OF
      #Production.confirmManufOrder(models_proxy,order_id)                                                                                        #Confirmation de l'OF
      #Production.DoneManufOrder(models_proxy,order_id,product_id,quantity)                                                                       #Ordre de fabrication terminé
