a3ad71573d2d0310e9a674bd62f12d4122840873
Mister-J23 <indrichjardel@gmail.com>
2024-01-22 15:56:34 +0100

    j

--- CodeInProgressFich/Production.py
+++ CodeInProgressFich/Production.py
@@ -6,8 +6,8 @@
 # Autor : L.P                                                         #
 #######################################################################
 #======================================================================
-
-import base64 , os
+from PIL import Image
+import base64 , os, io
 import xmlrpc.client
 import CodeStockageJardel
 
@@ -23,6 +23,40 @@ common_proxy = xmlrpc.client.ServerProxy(gUrl)
 gUid = common_proxy.authenticate("PokeFigDataBase", "paimblancleo@gmail.com", password, {})
 
 #======================================================================
+
+# Fonction pour récupérer la liste des produits
+def Product(models, gUid, password, database):
+    try:
+        product_ids = models.execute_kw(database, gUid, password,
+                                        'product.template', 'search_read',
+                                        [[]],
+                                        {'fields': ['default_code']})
+        return product_ids if product_ids else None
+    except Exception as e:
+        print(f"Erreur lors de la recherche des produits : {e}")
+        return None
+
+def ShowProductImage(self, models, db, uid, password, product_id):
+    try:
+        # Récupérer le produit avec l'identifiant product_id
+        product = models.execute_kw(
+            db, uid, password,
+            'product.template', 'read',
+            [product_id],
+            {'fields': ['image_1920']}
+        )
+
+        if product and product[0].get('image_1920'):
+            # Convertir la chaîne d'image base64 en bytes
+            image_bytes = base64.b64decode(product[0]['image_1920'])
+             # Créer l'objet Image à partir des bytes
+            image = Image.open(io.BytesIO(image_bytes))
+            return image
+        else:
+            print(f"Le produit avec l'ID {product_id} n'a pas d'image.")
+    except Exception as e:
+        print(f"Erreur lors de l'affichage de l'image du produit : {e}")
+
 
 def SaveProductImage(models, db, uid, password, product_id, save_path="/home/user/Documents/clone/CodePython/CodeInProgressFich/"):
     try:
